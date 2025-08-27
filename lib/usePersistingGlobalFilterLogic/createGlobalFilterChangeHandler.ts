e2c447afa0a988a727d4a3c6beb756af313c7ece
luriondel <lucasrndl@gmail.com>
2025-08-27 18:01:49 +0200

    refactor createGlobalFilterChangeHandler to improve error handling for updater function with undefined currentTableState

--- lib/usePersistingGlobalFilterLogic/createGlobalFilterChangeHandler.ts
+++ lib/usePersistingGlobalFilterLogic/createGlobalFilterChangeHandler.ts
@@ -11,7 +11,10 @@ export function createGlobalFilterChangeHandler(
   return (updater: Updater<string>, currentTableState?: string) => {
     const prev = currentTableState;
 
-    if (!prev && typeof updater === "function") {
+    if (
+      (prev === null || prev === undefined) &&
+      typeof updater === "function"
+    ) {
       throw new Error(
         "Cannot use updater function when currentTableState is undefined"
       );
